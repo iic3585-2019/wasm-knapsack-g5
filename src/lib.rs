50724f824d49505daf6a1b70991846e3665505c1
Francisco <fco.olivaress@gmail.com>
2019-04-14 19:48:27 -0400

    Fixes solver implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-mod utils;
 extern crate web_sys;
 
 use wasm_bindgen::prelude::*;
@@ -8,35 +7,6 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-
-// Next let's define a macro that's like `println!`, only it works for
-// `console.log`. Note that `println!` doesn't actually work on the wasm target
-// because the standard library currently just eats all output. To get
-// `println!`-like behavior in your app you'll likely want a macro like this.
-/*
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}*/
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
@@ -48,7 +18,6 @@ macro_rules! log {
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Item {
-    id: u32,
     name: char,
     value: u32,
     weight: u32,
@@ -56,9 +25,8 @@ pub struct Item {
 
 #[wasm_bindgen]
 impl Item {
-    pub fn new(new_id: u32, new_name: char, new_value: u32, new_weight: u32) -> Item {
+    pub fn new(new_name: char, new_value: u32, new_weight: u32) -> Item {
         Item {
-            id: new_id,
             name: new_name,
             value: new_value,
             weight: new_weight,
@@ -70,7 +38,7 @@ impl Item {
 pub struct Knapsack {
     maximun_weight: u32,
     items: Vec<Item>,
-    selected_items: Vec<Item>,
+    selected_items: Vec<u32>,
 }
 
 #[wasm_bindgen]
@@ -87,67 +55,107 @@ impl Knapsack {
         self.items.push(new_item);
     }
 
+    // Getter de los selected_items, que viene a ser el resultado final
+    pub fn get_selected_items(&self) -> Vec<u32> {
+        return self.selected_items.clone();
+    }
+
     pub fn solve_it(&mut self) {
         // Knapsack::show_items(&self.items);
 
         let n = self.items.len();
         let mut max = 0;
-        let mut best_subset = Vec::new();
+        let mut best_items = Vec::new();
         let mut eval;
 
-        for i in 0..n {
-            let mut sub_set = Vec::new();
-            sub_set.push(self.items[i].clone());
+        for i in 0..(i32::pow(2, n as u32) + 1) {
+            //log!("Voy en la iteración de {}", i);
+            let current_items = get_binary(i as u32, n as u32);
 
-            eval = Knapsack::evaluate(self, &sub_set);
-            // log!("[Afuera] El eval de {}, dio {}", self.items[i].id, eval);
+            eval = Knapsack::evaluate(self, &current_items);
             if eval > max {
                 max = eval;
-                best_subset = sub_set.clone();
-            }
-
-            for j in (i + 1)..n {
-                sub_set.push(self.items[j].clone());
-                eval = Knapsack::evaluate(self, &sub_set);
-                // log!("[Dentro] El eval de {}, dio {}", self.items[j].id, eval);
-                if eval > max {
-                    max = eval;
-                    best_subset = sub_set.clone();
-                }
+                best_items = current_items.clone();
             }
         }
 
-        log!("Se van a mostrar los id de los mejores items");
-        for i in 0..best_subset.len() {
-            log!("ID: {}", best_subset[i].id)
+        // Se asignan los items
+        for i in 0..best_items.len() {
+            if best_items[i] {
+                self.selected_items.push(1);
+            } else {
+                self.selected_items.push(0);
+            }
         }
     }
 }
 
+// Dado un número decimal retorna un vector con true o false
+// que lo representa en binario
+pub fn get_binary(input: u32, n_bits: u32) -> Vec<bool> {
+    let mut flag = true;
+    let mut number = input.clone();
+    let mut i = 0;
+    let mut binary = Vec::new();
+
+    while flag {
+        if number / 2 == 0 {
+            flag = false;
+        }
+
+        if number % 2 > 0 {
+            binary.push(true)
+        } else {
+            binary.push(false)
+        }
+
+        number = number / 2;
+
+        i += 1;
+    }
+
+    while (binary.len() as u32) < n_bits {
+        binary.push(false);
+    }
+
+    // Importante darlo vuelta para que los bits menos significativos queden a la derecha
+    binary.reverse();
+    return binary;
+}
+
 impl Knapsack {
-    pub fn evaluate(&self, arr: &[Item]) -> u32 {
+    // Evalua el arreglo de items dado un vector de true o false que dice si el item
+    // es seleccionado o no
+    pub fn evaluate(&self, selected_items: &[bool]) -> u32 {
         let mut current_weight = 0;
         let mut current_value = 0;
-        for i in 0..arr.len() {
-            current_weight += arr[i].weight;
+        for i in 0..selected_items.len() {
+            // Se ha seleccionado el item
+            if selected_items[i] {
+                // Se suma el peso
+                current_weight += self.items[i].weight;
 
-            if current_weight <= self.maximun_weight {
-                current_value += arr[i].value;
-            } else {
-                return 0;
+                // Se ve si el peso es válido
+                if current_weight <= self.maximun_weight {
+                    current_value += self.items[i].value;
+                // Si 'rompe' la mochila se retorna altiro
+                } else {
+                    return 0;
+                }
             }
         }
         return current_value;
     }
 
+    // Función que muestra un arreglo de items
     pub fn show_items(arr: &[Item]) {
         for i in 0..arr.len() {
             log!(
-                "ID: {}, valor: {}, peso {}",
-                arr[i].id,
+                "Indice: {}, Valor: {}, peso {}",
+                i,
                 arr[i].value,
                 arr[i].weight
-            )
+            );
         }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,34 +1,75 @@
 import * as wasm from "wasm-game-of-life";
 
-//wasm.greet("Usuario nuevo");
-
 const ITEMS = [
     {
-        id: 1,
+        id: 0,
         name: "Leche",
-        value: 600,
-        weight: 2
+        weight: 23,
+        value: 92
+    },
+    {
+        id: 1,
+        name: "Oro",
+        weight: 31,
+        value: 57
     },
     {
         id: 2,
-        name: "Oro",
-        value: 1000,
-        weight: 10
+        name: "Palta",
+        weight: 29,
+        value: 49
     },
     {
         id: 3,
+        name: "Tomate",
+        weight: 44,
+        value: 68
+    },
+    {
+        id: 4,
+        name: "Yogurt",
+        weight: 53,
+        value: 60
+    },
+    {
+        id: 5,
+        name: "Empanada",
+        weight: 38,
+        value: 43
+    },
+    {
+        id: 6,
         name: "Palta",
-        value: 900,
-        weight: 5
+        weight: 63,
+        value: 67
+    },
+    {
+        id: 7,
+        name: "Palta",
+        weight: 85,
+        value: 84
+    },
+    {
+        id: 8,
+        name: "Palta",
+        weight: 89,
+        value: 87
+    },
+    {
+        id: 9,
+        name: "Palta",
+        weight: 82,
+        value: 72
     }
 ]
 
-const MAX_WEIGHT = 15
+const MAX_VALUE = 165
 
-let knapsack = wasm.Knapsack.new(MAX_WEIGHT)
+let knapsack = wasm.Knapsack.new(MAX_VALUE)
 
 ITEMS.forEach((item) => {
-    knapsack.append_item(wasm.Item.new(item.id, item.name, item.value, item.weight))
+    knapsack.append_item(wasm.Item.new(item.name, item.value, item.weight))
 })
 
 knapsack.solve_it()
+console.log(knapsack.get_selected_items())
