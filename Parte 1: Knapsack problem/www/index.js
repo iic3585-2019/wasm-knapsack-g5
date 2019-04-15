d7e3b5135fa26150a1e1bef9c9c343f8b9ba6246
Francisco <fco.olivaress@gmail.com>
2019-04-15 16:08:29 -0400

    Adds timer

--- Parte 1: Knapsack problem/www/index.js
+++ Parte 1: Knapsack problem/www/index.js
@@ -3,66 +3,60 @@ import * as wasm from "knapsack-problem";
 
 const ITEMS = [
     {
-        id: 0,
         name: "Leche",
         weight: 23,
         value: 92
     },
     {
-        id: 1,
         name: "Oro",
         weight: 31,
         value: 57
     },
     {
-        id: 2,
         name: "Palta",
         weight: 29,
         value: 49
     },
     {
-        id: 3,
         name: "Tomate",
         weight: 44,
         value: 68
     },
     {
-        id: 4,
         name: "Yogurt",
         weight: 53,
         value: 60
     },
     {
-        id: 5,
         name: "Empanada",
         weight: 38,
         value: 43
     },
     {
-        id: 6,
         name: "Palta",
         weight: 63,
         value: 67
     },
     {
-        id: 7,
         name: "Palta",
         weight: 85,
         value: 84
     },
     {
-        id: 8,
         name: "Palta",
         weight: 89,
         value: 87
     },
     {
-        id: 9,
         name: "Palta",
         weight: 82,
         value: 72
     }
 ]
+
+for (let i = 0; i < 10; i++) {
+    ITEMS.push({ name: "Fake", weight: 1, value: 1 })
+}
 
 const MAX_VALUE = 165
 
@@ -72,5 +66,20 @@ ITEMS.forEach((item) => {
     knapsack.append_item(wasm.Item.new(item.name, item.value, item.weight))
 })
 
+const timer = name => {
+    let start = new Date();
+    return {
+        stop: function () {
+            let end = new Date();
+            let time = end.getTime() - start.getTime();
+            console.log('Timer:', name, 'finished in', time, 'ms');
+        }
+    }
+}
+
+let a = timer("Resolución")
+
 knapsack.solve_it()
+
+a.stop()
 console.log(knapsack.get_selected_items())
